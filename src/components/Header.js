766fd9ed2901b03e41fb32b4eeb942156a00ec75
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-01-29 23:33:29 -0500

    Portfolio updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,101 +4,107 @@ import { Link } from "react-scroll";
 import { gsap } from "gsap";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [navClass, setNavClass] = useState("");
+	const [isOpen, setIsOpen] = useState(false);
+	const [navClass, setNavClass] = useState("");
 
-  const toggleMenu = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+	const toggleMenu = () => {
+		setIsOpen((prevIsOpen) => !prevIsOpen);
 
-    const maxHeight = isOpen ? "-10px" : "300px";
+		const maxHeight = isOpen ? "-10px" : "300px";
 
-    gsap.to("nav ul", {
-      maxHeight,
-      duration: 0.4,
-      ease: "power2.out",
-    });
+		gsap.to("nav ul", {
+			maxHeight,
+			duration: 0.4,
+			ease: "power2.out",
+		});
 
-    if (isOpen) {
-      setTimeout(() => {
-        setNavClass("");
-      }, 180);
-    } else setNavClass("open");
-  };
+		if (isOpen) {
+			setTimeout(() => {
+				setNavClass("");
+			}, 180);
+		} else setNavClass("open");
+	};
 
-  return (
-    <header className="cyberpunk-header">
-      <div className="container">
-        <h1 className="glitch-text">Sunehildeep Singh</h1>
-        <nav className={navClass}>
-          <div className="menu-toggle" onClick={toggleMenu}>
-            <div className="hamburger"></div>
-          </div>
-          <ul>
-            <li>
-              <Link
-                to="about"
-                smooth={true}
-                duration={500}
-                offset={-100}
-                onClick={toggleMenu}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="goal"
-                smooth={true}
-                duration={500}
-                offset={-100}
-                onClick={toggleMenu}
-              >
-                Goal
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="projects"
-                smooth={true}
-                duration={500}
-                offset={-120}
-                onClick={toggleMenu}
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <a href="https://github.com/Sunehildeep" onClick={toggleMenu}>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <Link
-                to="skills"
-                smooth={true}
-                duration={500}
-                offset={-100}
-                onClick={toggleMenu}
-              >
-                Skills
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="contact"
-                smooth={true}
-                duration={500}
-                offset={-100}
-                onClick={toggleMenu}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+	return (
+		<header className="cyberpunk-header">
+			<div className="container">
+				<img
+					id="userpic"
+					src="/images/me.jpg"
+					alt="Header Profile"
+					className="header-profile-image animate-on-scroll"
+				/>
+				<h1 className="glitch-text">Sunehildeep Singh</h1>
+				<nav className={navClass}>
+					<div className="menu-toggle" onClick={toggleMenu}>
+						<div className="hamburger"></div>
+					</div>
+					<ul>
+						<li>
+							<Link
+								to="about"
+								smooth={true}
+								duration={500}
+								offset={-100}
+								onClick={toggleMenu}
+							>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="goal"
+								smooth={true}
+								duration={500}
+								offset={-100}
+								onClick={toggleMenu}
+							>
+								Goal
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="projects"
+								smooth={true}
+								duration={500}
+								offset={-120}
+								onClick={toggleMenu}
+							>
+								Projects
+							</Link>
+						</li>
+						<li>
+							<a href="https://github.com/Sunehildeep" onClick={toggleMenu}>
+								GitHub
+							</a>
+						</li>
+						<li>
+							<Link
+								to="skills"
+								smooth={true}
+								duration={500}
+								offset={-100}
+								onClick={toggleMenu}
+							>
+								Skills
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="contact"
+								smooth={true}
+								duration={500}
+								offset={-100}
+								onClick={toggleMenu}
+							>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Controller, Scene } from "scrollmagic";
 import CyberpunkBanner from "../../components/CyberpunkBanner";
 import { gsap } from "gsap";
@@ -6,82 +6,77 @@ import { titleDescription } from "./constants";
 import Section from "../../components/Section";
 
 const Home = () => {
-  const controller = useRef(null);
+	const controller = useRef(null);
 
-  useEffect(() => {
-    // Initialize ScrollMagic controller
-    controller.current = new Controller();
+	useEffect(() => {
+		// Initialize ScrollMagic controller
+		controller.current = new Controller();
 
-    // Add the ScrollMagic library initialization code
-    const scrollMagicController = controller.current;
-    const scrollMagicElements = document.querySelectorAll(".animate-on-scroll");
+		// Add the ScrollMagic library initialization code
+		const scrollMagicController = controller.current;
+		const scrollMagicElements = document.querySelectorAll(".animate-on-scroll");
 
-    // Another scene for userpic
-    new Scene({
-      triggerElement: "#userpic",
-      triggerHook: 0.8,
-      reverse: true,
-    })
-      .on("enter", () => {
-        gsap.to("#userpic", { opacity: 1, scale: 1.0, duration: 1 });
-      })
-      .on("leave", () => {
-        gsap.to("#userpic", { opacity: 0, scale: 0, duration: 1 });
-      })
-      .addTo(scrollMagicController);
+		// Another scene for userpic
+		new Scene({
+			triggerElement: "#userpic",
+			triggerHook: 0.8,
+			reverse: true,
+		})
+			.on("enter", () => {
+				gsap.to("#userpic", { opacity: 1, scale: 1.0, duration: 1 });
+			})
+			.on("leave", () => {
+				gsap.to("#userpic", { opacity: 0, scale: 0, duration: 1 });
+			})
+			.addTo(scrollMagicController);
 
-    // Slow loading for project
-    const projectElements = document.querySelectorAll(".project");
-    projectElements.forEach((element) => {
-      new Scene({
-        triggerElement: element,
-        triggerHook: 1.0,
-        reverse: true,
-      })
-        .on("enter", () => {
-          gsap.to(element, { opacity: 1, y: 0, x: 0, duration: 0.2 });
-        })
-        .on("leave", () => {
-          gsap.to(element, { opacity: 0, y: 100, x: -100, duration: 0.2 });
-        })
-        .addTo(scrollMagicController);
-    });
+		// Slow loading for project
+		const projectElements = document.querySelectorAll(".project");
+		projectElements.forEach((element) => {
+			new Scene({
+				triggerElement: element,
+				triggerHook: 1.0,
+				reverse: true,
+			})
+				.on("enter", () => {
+					gsap.to(element, { opacity: 1, y: 0, x: 0, duration: 0.2 });
+				})
+				.on("leave", () => {
+					gsap.to(element, { opacity: 0, y: 100, x: -100, duration: 0.2 });
+				})
+				.addTo(scrollMagicController);
+		});
 
-    scrollMagicElements.forEach((element) => {
-      new Scene({
-        triggerElement: element,
-        triggerHook: 0.8,
-        reverse: true,
-      })
-        .on("enter", () => {
-          gsap.to(element, { opacity: 1, y: 0, x: 0, duration: 1 });
-        })
-        .on("leave", () => {
-          gsap.to(element, { opacity: 0, y: 100, x: -100, duration: 1 });
-        })
-        .addTo(scrollMagicController);
-    });
+		scrollMagicElements.forEach((element) => {
+			new Scene({
+				triggerElement: element,
+				triggerHook: 0.8,
+				reverse: true,
+			})
+				.on("enter", () => {
+					gsap.to(element, { opacity: 1, y: 0, x: 0, duration: 1 });
+				})
+				.on("leave", () => {
+					gsap.to(element, { opacity: 0, y: 100, x: -100, duration: 1 });
+				})
+				.addTo(scrollMagicController);
+		});
 
-    return () => {
-      // Clean up ScrollMagic controller when component unmounts
-      controller.current.destroy(true);
-    };
-  }, []);
+		return () => {
+			// Clean up ScrollMagic controller when component unmounts
+			controller.current.destroy(true);
+		};
+	}, []);
 
-  return (
-    <>
-      <CyberpunkBanner />
+	return (
+		<>
+			<CyberpunkBanner />
 
-      {titleDescription.map((item, index) => {
-        return (
-          <Section
-            key={index}
-            {...item}
-          />
-        );
-      })}
-    </>
-  );
+			{titleDescription.map((item, index) => {
+				return <Section key={index} {...item} />;
+			})}
+		</>
+	);
 };
 
 export default Home;
