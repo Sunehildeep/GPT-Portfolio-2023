c761f1416b74ed9d7aef9bf59481e5d20b9a25a4
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-05-06 08:59:44 -0400

    Analyltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Inter } from 'next/font/google';
-import '../styles/styles.css';
-import Script from 'next/script'
+import React from "react";
+import { Inter } from "next/font/google";
+import "../styles/styles.css";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Sunehildeep Singh',
-    description: 'Portfolio of Sunehildeep Singh',
+	title: "Sunehildeep Singh",
+	description: "Portfolio of Sunehildeep Singh",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode;}) => {
-    return (
-        <html lang="en">
-            <head>
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-20H3WVHZW4"></Script>
-                <Script>
-                    {window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<html lang="en">
+			<head>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-20H3WVHZW4"
+				/>
+				<Script id="gtag">
+					{`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
                 
-                  gtag('config', 'G-20H3WVHZW4');}
-                </Script>
-            </head>
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+                    gtag('config', 'G-20H3WVHZW4');`}
+				</Script>
+			</head>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 };
 
 export default RootLayout;
