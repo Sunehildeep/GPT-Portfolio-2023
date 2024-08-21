b3e023e8c3213978d7ba8f5fe54e81496c5d41c3
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-08-21 16:39:56 -0400

    Build Types Fix

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -28,49 +28,55 @@ type Section = {
 	description: string | JSX.Element;
 	showAvatar?: boolean;
 };
-const timelineData = [
+
+type Timeline = {
+	year: string;
+	content: string;
+};
+
+const timelineData: Timeline[] = [
 	{
-		year: 2016,
+		year: "2016",
 		content:
 			"At 14, I learned my first programming language, Pawn, used for SA:MP servers, within just 3 months. Shortly after, I also picked up PHP.",
 	},
 	{
-		year: 2017,
+		year: "2017",
 		content:
 			"I became a reputable member of the SA:MP community, contributing by helping others and creating content. I established my own community, which grew to 100+ members, and managed a team of Admins, Managers, Moderators, Testers, Designers, and Programmers. I also created my first GitHub repository and expanded my skill set by learning Python, C#, Java, and JavaScript. I developed websites, user control panels, game scripts, and made significant community contributions. I also learned Photoshop and video editing, honing my skills in leadership, marketing, programming, and design.",
 	},
 	{
-		year: 2018,
+		year: "2018",
 		content:
 			"I created widely-used scripts for the SA:MP community and launched a startup called Honor Gaming Studios with online friends. Although we had to shut it down due to school commitments, the experience was incredibly valuable as I had learned Game Programming as well. Later that year, I began learning Docker, Golang, and TensorFlow as I delved into AI. I also started making Custom Windows Images specially tweaked for gaming and performance by researching improvements and released them on TeamOS.",
 	},
 	{
-		year: 2019,
+		year: "2019",
 		content:
 			"By this time, I had explored various fields and gained experience in all of them. I was still developing custom images for Windows on TeamOS and gained a huge reputation there, with my work being reposted across several websites and YouTube channels. Later, I started a YouTube channel focused on PC tweaks, reaching a point where I could monetize it. In my final years of high school, my skills were widely recognized, making me popular both for my YouTube channel and my programming abilities. I even worked for my school's IT department.",
 	},
 	{
-		year: 2020,
+		year: "2020",
 		content:
 			"During the COVID pandemic, I contributed to a Dockergo project on GitHub which was later archived into GitHub Arctic Code Vault, earning me an Arctic Code Contributor badge. I was invited to Google's secret hiring process known as 'Foobar,' a covert recruitment method used to identify top developers worldwide. Although I saved the invitation for potential future use, I did not participate at that time. Later, I freelanced for a friend's immigration company, developing a C# IELTS training software, and automated my home with a Raspberry Pi.",
 	},
 	{
-		year: 2021,
+		year: "2021",
 		content:
 			"In October, I moved to Canada and enrolled at Centennial College to study AI.",
 	},
 	{
-		year: 2022,
+		year: "2022",
 		content:
 			"With studies being online, I focused on personal projects and developed 'Custom Fan Helios,' a project for Acer laptops that gained significant traction. It was shared across YouTube, Discord, and Reddit, earning me community contributor status. I also worked on building my portfolio.",
 	},
 	{
-		year: 2023,
+		year: "2023",
 		content:
 			"At 20, I landed my first Software Engineering job at Sun Glow Window Coverings in January which was a huge achievement for me as a young individual. I developed an Encoder-Decoder transformer model from scratch, inspired by Google's research paper, and spent much of my time reading and researching to deepen my understanding of AI.",
 	},
 	{
-		year: 2024,
+		year: "2024",
 		content:
 			"I have worked over a year, overseeing all development projects. I spearheaded the creation of two major websites, developed a machine learning model, and designed several Python automation scripts. My work in automating processes has significantly improved efficiency across the company. As I continue to innovate and lead the team, I look forward to tackling new challenges and driving further success.",
 	},
@@ -89,7 +95,7 @@ export default function Home() {
 						This is what makes me who I am today.
 					</h3>
 					<VerticalTimeline>
-						{timelineData.map((data, index) => (
+						{timelineData.map((data: Timeline, index: number) => (
 							<VerticalTimelineElement
 								key={index}
 								className="vertical-timeline-element--work"
@@ -116,9 +122,6 @@ export default function Home() {
 					<div className="flex justify-center">
 						<Link
 							href="/images/resume.pdf"
-							smooth={true}
-							duration={500}
-							offset={-200}
 							className="cursor-pointer my-8 text-blue-500 bg-transparent border border-solid border-blue-500 hover:bg-blue-500 hover:text-white active:bg-blue-600 font-bold uppercase text-sm px-20 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 						>
 							Resume
