1af51704ee9d9939e78d7c736aa62a8a7cb4d6dd
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-09-17 12:08:12 -0400

    Description update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,52 +7,61 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 const personSchema = {
-		"@context": "https://schema.org",
-		"@type": "Person",
-		name: "Sunehildeep Singh",
-		jobTitle: "AI Engineer",
-		affiliation: {
-			"@type": "Organization",
-			name: "Sun Glow Window Coverings",
-		},
-		alumniOf: {
-			"@type": "CollegeOrUniversity",
-			name: "Centennial College",
-		},
-		worksFor: {
-			"@type": "Organization",
-			name: "Sun Glow Window Coverings",
-		},
-		sameAs: [
-			"https://www.linkedin.com/in/sunehildeepsingh",
-			"https://github.com/Sunehildeep",
-			"https://sunehildeepsingh.com",
-			"https://instagram.com/sunehildeep",
-			"https://x.com/Sunehildeep",
-		],
-		birthDate: "2002-02-28",
-		birthPlace: {
-			"@type": "Place",
-			name: "Kapurthala, India",
-		},
-		nationality: "Indian",
-		gender: "Male",
-		knowsAbout: [
-			"Artificial Intelligence",
-			"Deep Learning",
-			"Neural Networks",
-			"Convolutional Neural Networks (CNNs)",
-			"Recurrent Neural Networks (RNNs)",
-			"Python",
-			"JavaScript",
-			"TensorFlow",
-			"PyTorch",
-		],
+	"@context": "https://schema.org",
+	"@type": "Person",
+	name: "Sunehildeep Singh",
+	jobTitle: "AI Engineer",
+	givenName: "Sunehildeep",
+	affiliation: {
+		"@type": "Organization",
+		name: "Sun Glow Window Coverings",
+	},
+	alumniOf: {
+		"@type": "CollegeOrUniversity",
+		name: "Centennial College",
+	},
+	worksFor: {
+		"@type": "Organization",
+		name: "Sun Glow Window Coverings",
+	},
+	sameAs: [
+		"https://www.linkedin.com/in/sunehildeepsingh",
+		"https://github.com/Sunehildeep",
+		"https://sunehildeepsingh.com",
+		"https://instagram.com/sunehildeep",
+		"https://x.com/Sunehildeep",
+	],
+	birthDate: "2002-02-28",
+	birthPlace: {
+		"@type": "Place",
+		name: "Kapurthala, India",
+	},
+	nationality: "Indian",
+	gender: "Male",
+	knowsAbout: [
+		"Artificial Intelligence",
+		"Deep Learning",
+		"Neural Networks",
+		"Convolutional Neural Networks (CNNs)",
+		"Recurrent Neural Networks (RNNs)",
+		"Python",
+		"JavaScript",
+		"TensorFlow",
+		"PyTorch",
+	],
+	image: "https://sunehildeepsingh.com/images/me_full.jpg",
+	description: `Sunehildeep Singh is an innovative AI Engineer with a deep passion for advancing the field of artificial intelligence through neural networks and deep learning. Starting his programming journey at a young age of 14, Sunehildeep has continually expanded his skills and expertise across various domains of technology.
+
+With a strong foundation in software engineering and a keen interest in AI, Sunehildeep has dedicated himself to mastering the intricacies of deep learning. His work is characterized by a commitment to exploring the latest advancements in neural network architectures and developing cutting-edge solutions that address complex challenges.
+
+Graduating from Centennial College with a focus on Artificial Intelligence, Sunehildeep has leveraged his academic background to create impactful projects and contribute to the field. His expertise extends to developing sophisticated deep learning models and utilizing advanced techniques to drive innovation.
+
+Sunehildeep’s journey is marked by a continuous pursuit of knowledge and excellence, with a focus on harnessing the power of AI to push the boundaries of technology. As he continues to explore new frontiers in neural networks and deep learning, Sunehildeep remains dedicated to shaping the future of AI and inspiring others in the industry.
+
+For insights into his work and to connect with him, visit his official website and follow his updates on LinkedIn, GitHub, and X.`,
 };
 
 export async function generateMetadata() {
-	
-
 	return {
 		title: "Sunehildeep Singh - AI Engineer",
 		description:
@@ -88,7 +97,7 @@ export async function generateMetadata() {
 				content:
 					"AI Engineer, Deep Learning, Neural Networks, Artificial Intelligence, Technology, Innovation",
 			},
-                ]
+		],
 	};
 }
 
@@ -100,11 +109,11 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-			<Script
-				id="schema"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
-      />
+				<Script
+					id="schema"
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
+				/>
 			</head>
 			<body className={`${inter.className} dark`}>
 				<NextUIProvider>{children}</NextUIProvider>
